da7bd55f9697e5bd553814c849d043f49c0bdb49
Asad <asad200ana@gmail.com>
2022-10-09 18:31:34 +0530

    Python fractals/trees

---
+++ Fractal Snowflake(Python)/snowflake.py
@@ -0,0 +1,30 @@
+from turtle import *
+
+shape("turtle")
+speed(0)
+
+def snowflake_side(length, levels):
+	if levels == 0:
+		forward(length)
+		return
+
+	length /= 3
+	snowflake_side(length, levels - 1)
+	left(60)
+	snowflake_side(length, levels - 1)
+	right(120)
+	snowflake_side(length, levels - 1)
+	left(60)
+	snowflake_side(length, levels - 1)
+
+
+def create_snowflake(sides, length):
+	colors = ["blue", "green", "black", "orange"]
+	for i in range(sides):
+		color(colors[i])
+		snowflake_side(length, sides)
+		right(360 / sides)
+
+create_snowflake(4, 200)
+
+mainloop()
